20db613835acc7dc6b81b40defb464a2a9072f5b
nguyen-osc <nguyentt@smartosc.com>
2025-10-20 17:03:39 +0700

    add react hook form

---
+++ src/components/form/BugReportForm.tsx
@@ -0,0 +1,164 @@
+"use client";
+import React from "react";
+import { Input } from "../ui/input";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "../ui/field";
+import { Controller, useForm } from "react-hook-form";
+import * as z from "zod";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroupTextarea,
+} from "../ui/input-group";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { Button } from "../ui/button";
+import { formSchema } from "./formSchema";
+
+export const BugReportForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      assigner: "",
+    },
+  });
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log("data", data);
+    toast("You submitted the following values:", {
+      description: (
+        <pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
+          <code>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+      position: "bottom-right",
+      classNames: {
+        content: "flex flex-col gap-2",
+      },
+      style: {
+        "--border-radius": "calc(var(--radius)  + 4px)",
+      } as React.CSSProperties,
+    });
+  }
+  return (
+    <Card className="mt-10  md:mt-20 max-w-[90%] mx-auto rounded-none">
+      <CardHeader>
+        <CardTitle>Bug Report</CardTitle>
+        <CardDescription>
+          Help us improve by reporting bugs you encounter.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form id="form-rhf-demo" onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup>
+            <Controller
+              name="title"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-demo-title">
+                    Bug Title
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    id="form-rhf-demo-title"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Login button not working on mobile"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="assigner"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-demo-assigner">
+                    Assigner
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    id="form-rhf-demo-assigner"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Assigner of this work"
+                    autoComplete="off"
+                  />
+
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="description"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-demo-description">
+                    Description
+                  </FieldLabel>
+                  <InputGroup>
+                    <InputGroupTextarea
+                      {...field}
+                      id="form-rhf-demo-description"
+                      placeholder="I'm having an issue with the login button on mobile."
+                      rows={6}
+                      className="min-h-24 resize-none"
+                      aria-invalid={fieldState.invalid}
+                    />
+                    <InputGroupAddon align="block-end">
+                      <InputGroupText className="tabular-nums">
+                        {field.value.length}/100 characters
+                      </InputGroupText>
+                    </InputGroupAddon>
+                  </InputGroup>
+                  <FieldDescription>
+                    Include steps to reproduce, expected behavior, and what
+                    actually happened.
+                  </FieldDescription>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Field orientation="horizontal">
+          <Button
+            type="button"
+            className="border-2 "
+            onClick={() => form.reset()}
+          >
+            Reset
+          </Button>
+          <Button type="submit" form="form-rhf-demo">
+            Submit
+          </Button>
+        </Field>
+      </CardFooter>
+    </Card>
+  );
+};
--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -5,42 +5,44 @@ import { usePathname } from "next/navigation";
 import { useEffect, useState, useRef } from "react";
 import { FaSearch } from "react-icons/fa";
 import styles from "./Header.module.css";
-import { AppSidebar } from "../AppSidebar/AppSidebar";
-import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 const navLinks = [
   { href: "/", label: "Home" },
-  { href: "/categories", label: "Categories" },
-  { href: "/about", label: "About" },
+  { href: "/shopifyProducts", label: "Shopify Products" },
+  { href: "/table", label: "DataTable Shadcn" },
+  { href: "/ticket", label: "Ticket" },
+  { href: "/react-hook-form", label: "React Form" },
 ];
 export default function Header() {
   const pathname = usePathname();
   const [isSearching, setIsSearching] = useState(false);
   const [scrolled, setScrolled] = useState(false);
-  const [open, setOpen] = useState(false)
-  const [hamburgerClicked, setHamburgerClicked] = useState(false)
-
-  const handleClick = ()=>{
-      setOpen(!open)
-      setHamburgerClicked(!hamburgerClicked)
-      console.log(open)
-
-  }
 
   useEffect(() => {
     const handleScroll = () => {
       setScrolled(window.scrollY > 100);
     };
-
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
   return (
-    <nav className={`${styles.header} ${scrolled ? styles.scrolled : "" } ${hamburgerClicked ? styles.hamburgerClicked : ""}`}>
+    <nav className={`${styles.header} ${scrolled ? styles.scrolled : ""} `}>
       <div>
         <ul className={styles.navList}>
-
           <div className={styles.leftItems}>
             {navLinks.map((link) => (
               <li
@@ -62,19 +64,32 @@ export default function Header() {
           </li>
         </ul>
         <div className={styles.hamburger}>
-          <button onClick={()=>handleClick()} ><Menu/></button>
-          {open && (
-          <div className={styles.navListMobile}>
-            {navLinks.map((link) => (
-              <li
-                key={link.href}
-                className={styles.liMobile}
-              >
-                <Link href={link.href}>{link.label}</Link>
-              </li>
-            ))}
-          </div>
-          )}
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button className="bg-transparent text-black">
+                <Menu />
+              </Button>
+            </SheetTrigger>
+            <SheetContent
+              side="left"
+              className="max-w-2/3 pt-15 [&_button_svg]:hidden [&_button_span]:hidden [&_focus]:none  [&_button:focus]:ring-0"
+            >
+              <SheetHeader className="py-0">
+                <SheetTitle>MindFlow</SheetTitle>
+                <SheetDescription>
+                  Make changes to your profile here. Click save when you&apos;re
+                  done.
+                </SheetDescription>
+              </SheetHeader>
+              <div className={styles.navListMobile}>
+                {navLinks.map((link) => (
+                  <li key={link.href} className={styles.liMobile}>
+                    <Link href={link.href}>{link.label}</Link>
+                  </li>
+                ))}
+              </div>
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
 
