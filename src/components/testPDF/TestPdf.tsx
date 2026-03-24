8818650d40d94477503f8b22a70bcd903c806f66
nguyenttsmart <nguyentt@smartosc.com>
2026-03-24 11:06:24 +0700

    update file url

--- src/components/testPDF/TestPdf.tsx
+++ src/components/testPDF/TestPdf.tsx
@@ -30,7 +30,7 @@ export const TestPdf = () => {
             onLoad={() => setIsLoading(false)}
             key={"SG-DOC-1760684724"}
             data={
-              "https://ska.oa.osim-martech.com/files/private/document/SG-DOC-1760684724_9_1761205061_MENVTb0mNT.pdf?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9za2Eub2Eub3NpbS1tYXJ0ZWNoLmNvbS9maWxlcy9wcml2YXRlL2RvY3VtZW50L1NHLURPQy0xNzYwNjg0NzI0XzlfMTc2MTIwNTA2MV9NRU5WVGIwbU5ULnBkZiIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTc3NDMyNDczNn19fV19&Signature=eNkOawqFRiKNKjio9lXabmNBD0njgmctCK5%7ewY%7eBfG1y-eIB6fIS-nFBKYBCfaKZ8N6GK%7ewoEETPqyIUU44ishAqt8naksB5Eq9oFV5BwCbQ8cyp6yiZf%7e3WR8Q%7etsie1OvYCGC6giZg9XagxnSa4yymfXA214mIyYOuzap4qmd1aLecogWzQnvGIAHSMU900JCeAh%7eVea2VWOWsA2oAFBN6JZeq1n5DKMjHdldvlXCJyn5ERzuAAkizlFj6jfO5dl4gB-fQXKcdU9j2jxbKED-24FWbY5%7e3WICrL96yBTq8fEc6lY3fKnfNenzzIWlVp5UesCqlUoJgVTkRM4IoJw__&Key-Pair-Id=K1TMO9RMMDST50"
+              "https://ska.oa.osim-martech.com/files/private/document/SG-DOC-1760684724_9_1761205061_MENVTb0mNT.pdf?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9za2Eub2Eub3NpbS1tYXJ0ZWNoLmNvbS9maWxlcy9wcml2YXRlL2RvY3VtZW50L1NHLURPQy0xNzYwNjg0NzI0XzlfMTc2MTIwNTA2MV9NRU5WVGIwbU5ULnBkZiIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTc3NDMyNTc2OX19fV19&Signature=LXBtcdf6EfsggBJQ9PyoyOoShFQ%7eeWlLJhPpKoeVbdZCNziXfKRbMDoVuPE3TjyvutoC9TnNU-pRWZp3f1%7e4VfIHqeS2TzAdh0u4r-N3XqBZClsXeyQ61LvkY7oNK%7e%7eTPxUkpZ2CRCaLQj5kVcM1gpO-vQzoFHzDpNZ4D-F3uSVMvuvcnhhXsJBzVk0vKjMlOORVJ5rgIxDSHzrZxcBlp6I77AG%7ewtZlmyV4EVJH%7ey12K48W6jwepUBOvf2EGk8e-P1SmROA9bQcMPsMQqB6IxBopaEqNdF6YywsYdulAnPVCdXiUgSfUaO69J92ytNga8SbBwWany-ZJCVtJJy8jg__&Key-Pair-Id=K1TMO9RMMDST50"
             }
           />
         </DialogContent>
